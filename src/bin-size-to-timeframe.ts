d505cfd8e0db5a67ce86273c1775eeda43f529ec
Eric Crosson <eric.s.crosson@utexas.edu>
2019-04-04 20:41:00 -0500

    Use ow to validate input

--- src/bin-size-to-timeframe.ts
+++ src/bin-size-to-timeframe.ts
@@ -3,6 +3,8 @@
  * Convert a BitMEX bin-size to a Trading View timeframe
  */
 
+import ow from 'ow'
+
 
 /**
  * Convert BiMEX bin size to Trading View timeframe.
@@ -11,6 +13,12 @@
  * @returns Length of bin size as Trading View timeframe
  */
 export default function binSizeToTimeframe(binSize: '1m' | '5m' | '1h' | '1d'): string {
+    ow(binSize, ow.string.is(s =>
+                             s === '1m' ||
+                             s === '5m' ||
+                             s === '1h' ||
+                             s === '1d' ||
+                            `Expected ${s} to be a valid BitMEX bin-size (either '1m', '5m', '1h', or '1d')`))
     return {
         '1m': '1',
         '5m': '5',
