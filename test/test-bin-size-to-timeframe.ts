84b81e62f05478ada6dba460fdefe416ab8517f2
Eric Crosson <eric.s.crosson@utexas.edu>
2019-04-04 20:43:12 -0500

    Add test of ow

--- test/test-bin-size-to-timeframe.ts
+++ test/test-bin-size-to-timeframe.ts
@@ -4,6 +4,8 @@ import test from 'ava'
  * Library under test
  */
 
+type BinSize = '1m' | '5m' | '1h' | '1d'
+
 import binSizeToTimeframe from '../src/bin-size-to-timeframe'
 
 const transformsBinSizeToExpectedTimeframe = (t: any, binSize: '1m' | '5m' | '1h' | '1d', timeframe: string) => t.is(binSizeToTimeframe(binSize), timeframe)
@@ -13,3 +15,10 @@ test(transformsBinSizeToExpectedTimeframe, '1m', '1')
 test(transformsBinSizeToExpectedTimeframe, '5m', '5')
 test(transformsBinSizeToExpectedTimeframe, '1h', '1H')
 test(transformsBinSizeToExpectedTimeframe, '1d', '1D')
+
+test('should reject non-bin-sizes', t => {
+    const error = t.throws(() => {
+        binSizeToTimeframe('1H' as BinSize)
+    })
+    t.is(error.name, 'ArgumentError')
+})
